79559f9b70f036a0c32d43aa5ab27b2e72921e60
rghermi-owkin <ridouane.ghermi@owkin.com>
2021-11-29 12:01:30 +0100

    basic code organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+   name="histo-starterkit",
+   version="0.1",
+   packages=find_packages(),
+   description="Basic introduction to Histology.",
+   author="Ridouane GHERMI",
+   url="https://github.com/rghermi-owkin/histo_starterkit",
+   python_requires=">=3.8",
+)
